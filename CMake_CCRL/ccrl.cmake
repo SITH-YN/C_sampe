25280634a764c91230b2a357d6036aea32679004
9921349-Z100 <yota.nakamura@aisin.co.jp>
2024-01-12 15:30:13 +0900

    CCRL用CMakeファイル/VSCode用ビルドタスクファイル追加

--- CMake_CCRL/ccrl.cmake
+++ CMake_CCRL/ccrl.cmake
@@ -1,4 +1,5 @@
 ### BEGIN CMAKE_TOOLCHAIN_FILE
+set(CMAKE_CROSSCOMPILING "TRUE")
 # "Generic" is used when cross compiling
 set(CMAKE_SYSTEM_NAME Generic)
 # Optional, this one not so much
@@ -9,30 +10,28 @@ set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_FIND_ROOT_PATH "C:/Program Files (x86)/Renesas Electronics/CS+/CC/CC-RL/V1.12.01/bin")
 # CMake variables for compiler, assembler, native build system
 # Specify the C compiler
-set(CMAKE_C_COMPILER ccrl)
+set(CMAKE_C_COMPILER "C:/Program Files (x86)/Renesas Electronics/CS+/CC/CC-RL/V1.12.01/bin/ccrl.exe")
 set(CMAKE_C_COMPILER_ID RLC)
 set(CMAKE_C_COMPILER_ID_RUN TRUE)
 set(CMAKE_C_COMPILER_FORCED TRUE)
 # Specify the CPP compiler
-set(CMAKE_CXX_COMPILER ccrl)
+set(CMAKE_CXX_COMPILER "C:/Program Files (x86)/Renesas Electronics/CS+/CC/CC-RL/V1.12.01/bin/ccrl.exe")
 set(CMAKE_CXX_COMPILER_ID RLC)
 set(CMAKE_CXX_COMPILER_ID_RUN TRUE)
 set(CMAKE_CXX_COMPILER_FORCED TRUE)
 # Specify the ASM compiler
-set(CMAKE_ASM_COMPILER asrl)
+set(CMAKE_ASM_COMPILER "C:/Program Files (x86)/Renesas Electronics/CS+/CC/CC-RL/V1.12.01/bin/asrl.exe")
 set(CMAKE_ASM_COMPILER_ID RLA)
 set(CMAKE_ASM_COMPILER_ID_RUN TRUE)
 set(CMAKE_ASM_COMPILER_FORCED TRUE)
 # Specify the linker
-set(CMAKE_LINKER rlink)
+set(CMAKE_LINKER "C:/Program Files (x86)/Renesas Electronics/CS+/CC/CC-RL/V1.12.01/bin/rlink.exe")
 # Specify options for command
-#set(COMMON_OPTIONS "-isa=rxv2 -fpu")
-set(CMAKE_C_FLAGS "${COMMON_OPTIONS} -lang=c -include=\"../generate\" -define=DEBUG_CONSO
-LE -utf8 -outcode=utf8 -nomessage -debug -nologo")
-set(CMAKE_CXX_FLAGS "${COMMON_OPTIONS} -lang=cpp -include=\"../generate\" -define=DEBUG_CO
-NSOLE -utf8 -outcode=utf8 -nomessage -debug -nologo")
-set(CMAKE_ASM_FLAGS "${COMMON_OPTIONS} -include=\"../generate\" -utf8 -debug -nologo")
-set(CMAKE_EXE_LINKER_FLAGS "-subcommand=../Linker.tmp")
+set(COMMON_OPTIONS "-cpu=S2 -dev=C:/Program Files (x86)/Renesas Electronics/CS+/CC/Device/RL78/Devicefile/DR5F100LE.DVF -g")
+set(CMAKE_C_FLAGS "${COMMON_OPTIONS} -lang=c -utf8 -outcode=utf8 -P -preprocess=comment,line -refs_without_declaration")
+set(CMAKE_CXX_FLAGS "${COMMON_OPTIONS} -lang=cpp -include=\"../generate\" -define=DEBUG_CONSOLE -utf8 -outcode=utf8 -nomessage -debug -nologo")
+set(CMAKE_ASM_FLAGS "${COMMON_OPTIONS} -utf8")
+set(CMAKE_EXE_LINKER_FLAGS "-subcommand=../Linker.opt")
 # Specify the make
 set(CMAKE_MAKE_PROGRAM make)
 ### END CMAKE_TOOLCHAIN_FILE
---
+++ CMake_CCRL/src/test.c
@@ -0,0 +1,8 @@
+/*--FizzBuzz問題--*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+
+  return 0;
+}	
